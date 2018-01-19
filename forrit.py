e35dc5f996474b65dfa01d0ff2c6ed8009fdbeec
totsss <axetra@hotmail.com>
2018-01-19 13:57:52 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -14,7 +14,6 @@ def index():
     return """
     <body>
       <h1>halló heimur!</h1><img src='static/gandalfeinn.jpg'>
-      <a href='https://is.wikipedia.org/wiki/Steve_Jobs'>Steve Jobs</a>
       <a href='/steve/1'>test<a/>
       <a href='/steve/2'>biography<a/>
       <a href='/steve/3'>Error<a/>
@@ -26,23 +25,33 @@ def index():
 def steve(nr):
     if nr=='1':
         return """
-          <h1>Wow Þetta er ekki sama mynd marr</h1>
-          <h1>þetta er myndin :)</h1><img src='./static/gandalfeinn.jpg'>
+          <h1>test eitthvað :) nr.1</h1>
+                
+                <a href='/steve/1'>test<a/>
+                <a href='/steve/2'>biography<a/>
+                <a href='/steve/3'>Error<a/>
+                <a href='/'>forsida<a/>
         """
     if nr=='2':
         return """
-          <h1>Undirsíða 2</h1>
-          <h1>þetta er myndin :)</h1><img src='./static/gandalfeinn.jpg'>
+          <h1>Undirsíða biography</h1>
+          <h1>nr.2</h1>>
+                
+                <a href='/steve/1'>test<a/>
+                <a href='/steve/2'>biography<a/>
+                <a href='/steve/3'>Error<a/>
+                <a href='/'>forsida<a/>
         """
     if nr=='3':
         return """
-          <h1>Error This site can’t be reached.</h1>
+          <h1>Error This site can’t be reached. Error síða gg</h1>
+          <a href='/steve/1'>test<a/>
+          <a href='/steve/2'>biography<a/>
+          <a href='/steve/3'>Error<a/>
+          <a href='/'>forsida<a/>
+          <h1> :( </h1>
 
         """
-
-
-
-
-
-
+    
+#run(host='localhost',port=8080,debug=True)
 run(host='0.0.0.0',port=os.environ.get('PORT'))
